1f3b7c55a894e3889cbc7b3e027be5af03250cb2
maxwellscripts <caughtonstream@gmail.com>
2026-03-01 01:22:32 +0000

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6Ijk1NmE3NmMxLTQxZDMtNGYyNC1hMDEwLTExZDEyMGM3NGQ1MCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
